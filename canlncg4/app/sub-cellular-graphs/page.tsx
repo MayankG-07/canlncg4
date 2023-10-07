c010594dd92fac662b039ea521f58c936b17f1ca
Mayank Gulati <sagax.dg@gmail.com>
2023-10-08 00:18:42 +0530

    Update page.tsx

--- canlncg4/app/sub-cellular-graphs/page.tsx
+++ canlncg4/app/sub-cellular-graphs/page.tsx
@@ -15,7 +15,7 @@ const SubCellularPage = () => {
         </CardBody>
     </Card>
     <Card sx={{mt:5,mx:7}}>
-        <CardHeader sx={{fontSize:30, textAlign: "center"}}>Plot 1 - Cytoplasmic/Nuclear Localisation: RCI distribution (all cell types)</CardHeader>
+        <CardHeader sx={{fontSize:30, textAlign: "center"}}>Plot 2 - Cytoplasmic/Nuclear Localisation: RCI distribution (all cell types)</CardHeader>
         <CardBody sx={{textAlign:"center"}}>
             <Text sx={{fontSize:20}}>Note: In the next plot <code style={{color: "red"}}>n</code> indicates the total number of genes in each group and <code style={{color: "red"}}>m</code> the median RCI value per group. The group percentile corresponding to each gene is also displayed next to the gene point.</Text>
             <Image sx={{ml: 8,mt:5}} src={`/subcellular-graphs/${lncrna_name}_dist.png`} />
