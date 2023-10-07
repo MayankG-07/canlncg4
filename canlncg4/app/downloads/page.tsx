5bf6d8a0168023aba88e2dbd6c09a1c4f2792159
Anmol Kumar <anmol4979199@gmail.com>
2023-10-08 03:41:23 +0530

    final update

--- canlncg4/app/downloads/page.tsx
+++ canlncg4/app/downloads/page.tsx
@@ -1,14 +1,51 @@
-import { Card, CardHeader, CardBody } from "@chakra-ui/react";
+import { Card, CardHeader, CardBody, Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react";
 
 const Downloads = () => {
+  const cancerData = [
+  { name: "All cancer-LncRNA G4s data", downloadLink: "./downloads/All%20cancer-LncRNA%20G4s%20data.xlsx" },
+  { name: "Blood cancer-LncRNA G4s data", downloadLink: "./downloads/Blood%20cancer-LncRNA%20G4s%20data.xlsx" },
+  { name: "Bone cancer-LncRNA G4s data", downloadLink: "./downloads/Bone%20cancer-LncRNA%20G4s%20data.xlsx" },
+  { name: "Brain cancer-LncRNA G4s data", downloadLink: "./downloads/Brain%20cancer-LncRNA%20G4s%20data.xlsx" },
+  { name: "Breast cancer-LncRNA G4s data", downloadLink: "./downloads/Breast%20cancer-LncRNA%20G4s%20data.xlsx" },
+  { name: "Cervical cancer-LncRNA G4s data", downloadLink: "./downloads/Cervical%20cancer-LncRNA%20G4s%20data.xlsx" },
+  { name: "Colorectal cancer-LncRNA G4s data", downloadLink: "./downloads/Colorectal%20cancer-LncRNA%20G4s%20data.xlsx" },
+  { name: "Gastric cancer-LncRNA G4s data", downloadLink: "./downloads/Gastric%20cancer-LncRNA%20G4s%20data.xlsx" },
+  { name: "Head and neck cancer-LncRNA G4s data", downloadLink: "./downloads/Head%20and%20neck%20cancer-LncRNA%20G4s%20data.xlsx" },
+  { name: "Liver cancer-LncRNA G4s data", downloadLink: "./downloads/Liver%20cancer-LncRNA%20G4s%20data.xlsx" },
+  { name: "Lung cancer-LncRNA G4s data", downloadLink: "./downloads/Lung%20cancer-LncRNA%20G4s%20data.xlsx" },
+  { name: "Ovarian cancer-LncRNA G4s data", downloadLink: "./downloads/Ovarian%20cancer-LncRNA%20G4s%20data.xlsx" },
+  { name: "Prostate cancer-LncRNA G4s data", downloadLink: "./downloads/Prostate%20cancer-LncRNA%20G4s%20data.xlsx" },
+  { name: "Skin cancer-LncRNA G4s data", downloadLink: "./downloads/Skin%20cancer-LncRNA%20G4s%20data.xlsx" },
+  { name: "Testicular cancer-LncRNA G4s data", downloadLink: "./downloads/Testicular%20cancer-LncRNA%20G4s%20data.xlsx" },
+  { name: "Uterine cancer-LncRNA G4s data", downloadLink: "./downloads/Uterine%20cancer-LncRNA%20G4s%20data.xlsx" }
+];
+
   return (
-    <Card sx={{ mt: 5, mx: 7 }}>
-      <CardHeader sx={{ fontSize: 25 }}>Downloads</CardHeader>
+    <Card mt={5} mx={7}>
+      <CardHeader fontSize={25}>Downloads</CardHeader>
       <CardBody>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime, esse.
-        Praesentium magni ut, eligendi unde natus doloremque numquam similique!
-        Iusto similique aliquid, accusamus praesentium inventore harum nobis
-        voluptatibus ducimus iure?
+        <Table variant="striped">
+          <Thead>
+            <Tr>
+              <Th>Sr. No.</Th>
+              <Th>Name of Database</Th>
+              <Th>Download Link</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {cancerData.map((item, index) => (
+              <Tr key={index}>
+                <Td>{index + 1}</Td>
+                <Td>{item.name}</Td>
+                <Td>
+                  <a href={item.downloadLink} target="_blank" rel="noopener noreferrer">
+                    Download
+                  </a>
+                </Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
       </CardBody>
     </Card>
   );
--- canlncg4/app/help/page.tsx
+++ canlncg4/app/help/page.tsx
@@ -48,16 +48,236 @@ const Help = () => {
     Help Section
     </Text></CardHeader><CardBody>
         <OrderedList>
-  <ListItem>Lorem ipsum dolor sit amet</ListItem>
-  <ListItem>Consectetur adipiscing elit</ListItem>
-  <ListItem>Integer molestie lorem at massa</ListItem>
-  <ListItem>Facilisis in pretium nisl aliquet</ListItem>
+     <Stack direction="row">
+        <Card sx={{ mt: 0, ml: 0, mr: 5, width: "100%" }}>
+          <CardHeader sx={{ fontSize: 25, textAlign: "center" }}>
+            {/* Human - Male */}
+          </CardHeader>
+          <CardBody>
+            <Box
+              sx={{
+                position: "relative",
+                width: "100%",
+                mx: 0
+              }}
+              className={styles.parent}
+            >
+              <img src="/h1.png" alt="help1" width="100%" />
+            </Box>
+          </CardBody>
+        </Card>
+
+      </Stack>     
+  <ListItem> Navigation bar provides access to the main functions of the database.</ListItem>
+  <Stack direction="row">
+        <Card sx={{ mt: 0, ml: 0, mr: 5, width: "100%" }}>
+          <CardHeader sx={{ fontSize: 25, textAlign: "center" }}>
+            {/* Human - Male */}
+          </CardHeader>
+          <CardBody>
+            <Box
+              sx={{
+                position: "relative",
+                width: "100%",
+                mx: 0
+              }}
+              className={styles.parent}
+            >
+              <img src="/h2.png" alt="help1" width="100%" />
+            </Box>
+          </CardBody>
+        </Card>
+
+      </Stack>
+      <Stack direction="row">
+        <Card sx={{ mt: 0, ml: 0, mr: 5, width: "100%" }}>
+          <CardHeader sx={{ fontSize: 25, textAlign: "center" }}>
+            {/* Human - Male */}
+          </CardHeader>
+          <CardBody>
+            <Box
+              sx={{
+                position: "relative",
+                width: "100%",
+                mx: 0
+              }}
+              className={styles.parent}
+            >
+              <img src="/h3.png" alt="help1" width="100%" />
+            </Box>
+          </CardBody>
+        </Card>
+
+      </Stack>
+      <ListItem>. A quick search for lncRNA or cancer.
+</ListItem>
+
+  <ListItem>. Advanced search to search the lncRNA and/ or cancer using parameters including, expression pattern,
+transcript variants and coding ability.</ListItem>
+      <Stack direction="row">
+        <Card sx={{ mt: 0, ml: 0, mr: 5, width: "100%" }}>
+          <CardHeader sx={{ fontSize: 25, textAlign: "center" }}>
+            {/* Human - Male */}
+          </CardHeader>
+          <CardBody>
+            <Box
+              sx={{
+                position: "relative",
+                width: "100%",
+                mx: 0
+              }}
+              className={styles.parent}
+            >
+              <img src="/h4.png" alt="help1" width="100%" />
+            </Box>
+          </CardBody>
+        </Card>
+
+      </Stack>
+   <ListItem>Click the organ or the name to open their respective associated lncRNAs.</ListItem>
+<Stack direction="row">
+        <Card sx={{ mt: 0, ml: 0, mr: 5, width: "100%" }}>
+          <CardHeader sx={{ fontSize: 25, textAlign: "center" }}>
+            {/* Human - Male */}
+          </CardHeader>
+          <CardBody>
+            <Box
+              sx={{
+                position: "relative",
+                width: "100%",
+                mx: 0
+              }}
+              className={styles.parent}
+            >
+              <img src="/h5.png" alt="help1" width="100%" />
+            </Box>
+          </CardBody>
+        </Card>
+
+      </Stack>
+  <ListItem>Result of the searched lncRNA</ListItem>
+  <ListItem> Click Details to check the G4 prediction or Subcellular localization</ListItem>
+  <Stack direction="row">
+        <Card sx={{ mt: 0, ml: 0, mr: 5, width: "100%" }}>
+          <CardHeader sx={{ fontSize: 25, textAlign: "center" }}>
+            {/* Human - Male */}
+          </CardHeader>
+          <CardBody>
+            <Box
+              sx={{
+                position: "relative",
+                width: "100%",
+                mx: 0
+              }}
+              className={styles.parent}
+            >
+              <img src="/h6.png" alt="help1" width="100%" />
+            </Box>
+          </CardBody>
+        </Card>
+
+      </Stack>
+   <ListItem>List of transcript variants of search lncRNA.</ListItem>
+  <ListItem>Click View to select the G4 prediction tool.</ListItem>
+  <ListItem>Change the parameters for G4 prediction, if required and click Analyze.</ListItem>
+  <ListItem>Result of G4 prediction with types of G4</ListItem>
+  <Stack direction="row">
+        <Card sx={{ mt: 0, ml: 0, mr: 5, width: "100%" }}>
+          <CardHeader sx={{ fontSize: 25, textAlign: "center" }}>
+            {/* Human - Male */}
+          </CardHeader>
+          <CardBody>
+            <Box
+              sx={{
+                position: "relative",
+                width: "100%",
+                mx: 0
+              }}
+              className={styles.parent}
+            >
+              <img src="/h7.png" alt="help1" width="100%" />
+            </Box>
+          </CardBody>
+        </Card>
+
+      </Stack>
+ <ListItem>RCI and expression values of the searched lncRNA.</ListItem>
+  <ListItem>Reference lncRNAs.</ListItem>
+  <ListItem>RCI distribution of the searched lncRNA.</ListItem>
+  <Stack direction="row">
+        <Card sx={{ mt: 0, ml: 0, mr: 5, width: "100%" }}>
+          <CardHeader sx={{ fontSize: 25, textAlign: "center" }}>
+            {/* Human - Male */}
+          </CardHeader>
+          <CardBody>
+            <Box
+              sx={{
+                position: "relative",
+                width: "100%",
+                mx: 0
+              }}
+              className={styles.parent}
+            >
+              <img src="/h8.png" alt="help1" width="100%" />
+            </Box>
+          </CardBody>
+        </Card>
+
+      </Stack>
+      <Stack direction="row">
+        <Card sx={{ mt: 0, ml: 0, mr: 5, width: "100%" }}>
+          <CardHeader sx={{ fontSize: 25, textAlign: "center" }}>
+            {/* Human - Male */}
+          </CardHeader>
+          <CardBody>
+            <Box
+              sx={{
+                position: "relative",
+                width: "100%",
+                mx: 0
+              }}
+              className={styles.parent}
+            >
+              <img src="/h9.png" alt="help1" width="100%" />
+            </Box>
+          </CardBody>
+        </Card>
+
+      </Stack>
+   <ListItem>Enter the sequence of RNA for which G4 prediction is required.</ListItem>
+  <ListItem>Enter the parameters for G4 prediction</ListItem>
+  <ListItem>Click Analyze to predict the G4 formation potential.</ListItem>
+  <Stack direction="row">
+        <Card sx={{ mt: 0, ml: 0, mr: 5, width: "100%" }}>
+          <CardHeader sx={{ fontSize: 25, textAlign: "center" }}>
+            {/* Human - Male */}
+          </CardHeader>
+          <CardBody>
+            <Box
+              sx={{
+                position: "relative",
+                width: "100%",
+                mx: 0
+              }}
+              className={styles.parent}
+            >
+              <img src="/h10.png" alt="help1" width="100%" />
+            </Box>
+          </CardBody>
+        </Card>
+
+      </Stack>
+
+  <ListItem>Enter the sequence of RNA for which G4 prediction is required.</ListItem>
+  <ListItem>Enter the parameters for G4 prediction.</ListItem>
+  <ListItem>Click Analyze to predict the G4 formation potential.</ListItem>
+
 </OrderedList>
 <br />
 <br />
  <Stack direction="row">
         <Card sx={{ mt: 5, ml: 7, mr: 5, width: "100%" }}>
-          <CardHeader sx={{ fontSize: 25, textAlign: "center" }}>
+          <CardHeader sx={{ fontSize: 5, textAlign: "center" }}>
             {/* Human - Male */}
           </CardHeader>
           <CardBody>
